e3bc4e32611c491ce3de67e05cb6d2942aa8e097
Jacob <Jacob.knight101@gmail.com>
2020-09-22 14:13:48 +0100

    yargs init

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,21 @@
 const chalk = require('chalk')
+const { describe } = require('yargs')
 const yargs = require('yargs')
 const notes = require('./notes.js')
 
 // Yargs version
 yargs.version('0.0.5')
+
+// add todo
+yargs.command({
+    command: 'add',
+    describe: 'Adds a new todo',
+    handler: function () {
+        console.log('Adding todo!')
+    }
+})
+
+// add, remove, read, list 
 
 console.log(yargs.argv)
 
