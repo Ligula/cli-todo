74468bd75a5bc297504dd7ac667f45eddef108d7
Jacob Knight <j.knight2@lancaster.ac.uk>
2020-09-24 15:02:26 +0100

    Start of add functionality

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,8 @@
 const chalk = require('chalk')
-const { describe } = require('yargs')
 const yargs = require('yargs')
 const notes = require('./notes.js')
+
+console.log(chalk.blue.inverse('Running app.js\n'))
 
 // Yargs version
 yargs.version('0.0.5')
@@ -9,15 +10,57 @@ yargs.version('0.0.5')
 // add todo
 yargs.command({
     command: 'add',
-    describe: 'Adds a new todo',
-    handler: function () {
-        console.log('Adding todo!')
+    describe: 'adds a new todo',
+    builder: {
+        m: {
+            describe: 'Todo message',
+            demandOption: true,
+            type: 'string'
+        },
+        p: {
+            describe: 'Todo priority',
+            type: 'number'
+        }
+    },
+    handler: function (argv) {
+        var msg = chalk.green('Added todo ') + chalk.yellowBright.italic(argv.m) + chalk.green()
+        var priority = ''
+
+        if(argv.p) {
+            priority =  chalk.green(' with priority ') + chalk.red(argv.p)
+        } 
+        console.log(msg + priority )
     }
 })
 
-// add, remove, read, list 
+// remove todo 
+yargs.command({
+    command: 'remove',
+    describe: 'removes a todo',
+    handler: function () {
+        console.log(chalk.red('Removing todo!'))
+    }
+})
 
-console.log(yargs.argv)
+// list todos
+yargs.command({
+    command: 'list',
+    describe: 'lists todos',
+    handler: function () {
+        console.log('test')
+    }
+})
+
+// read todos
+yargs.command({
+    command: 'read',
+    describe: 'read todos',
+    handler: function () {
+        console.log('')
+    }
+})
+
+yargs.parse()
 
 
 
